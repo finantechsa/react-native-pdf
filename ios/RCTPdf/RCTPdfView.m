91f8b69f3e3e52659507f10d15764f8143561092
nmagalhaes <53005565+nmagalhaes@users.noreply.github.com>
2023-06-23 16:51:54 +0100

    Update RCTPdfView.m to change backgroudColor  on iOS

--- ios/RCTPdf/RCTPdfView.m
+++ ios/RCTPdf/RCTPdfView.m
@@ -237,7 +237,7 @@ const float MIN_SCALE = 1.0f;
             }
         }
         
-        
+        _pdfView.backgroundColor = [UIColor clearColor];
         [_pdfView layoutDocumentView];
         [self setNeedsDisplay];
     }
